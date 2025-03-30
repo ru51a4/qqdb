e93353fb21dff6a927fa716eb6d0f10fbab703f9
ru51a4 <rruslanvaleev@yandex.ru>
2025-03-30 15:11:12 +0500

    fix

--- src/engine.js
+++ src/engine.js
@@ -67,7 +67,7 @@ export default class mysql {
                             } else {
                                 right = right.args
                             }
-                            if (!right.includes(String(left))) {
+                            if (!right.map((c) => String(c)).includes(String(left))) {
                                 arr[j].val = 0
                             } else {
                                 arr[j].val = 1
