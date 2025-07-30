29a4c5c5436d6a1d642c6e06f1b623c83c582de9
ru51a4 <rruslanvaleev@yandex.ru>
2025-07-30 23:49:03 +0500

    php sqlite benchmark

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("")
+let counter = 0;
+for (let i = 0; i <= a.length - 1; i++) {
+    let min = a[i]
+    let x = i
+    for (let j = i; j <= a.length - 1; j++) {
+        if (min > a[j]) {
+            min = a[j];
+            x = j;
+        }
+    }
+    console.log(min)
+    if (x !== i) {
+        let t = a[i]
+        a[i] = a[x];
+        a[x] = t;
+        counter++;
+        counter++;
+    }
+}
